c71ac8c3f098019827b93ba83f90c5c711802207
Yan Carlos <urbanoyaan@gmail.com>
2022-07-19 22:50:16 -0500

    Adding Files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'violet': '#1E0E62',
+        'page': '#2F1893',
+        'emerald': '#25DAC5',
+      },
+    },
+  },
+  plugins: [],
+}
